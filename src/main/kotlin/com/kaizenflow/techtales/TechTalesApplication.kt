3b985cf40cfc1835216d2ec01fa1207ece5a362e
Ahmed Marzook <m4marzook@gmail.com>
2024-10-09 21:33:55 +0100

    Initial Commit
    First Commit

---
+++ src/main/kotlin/com/kaizenflow/techtales/TechTalesApplication.kt
@@ -0,0 +1,11 @@
+package com.kaizenflow.techtales
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechTalesApplication
+
+fun main(args: Array<String>) {
+	runApplication<TechTalesApplication>(*args)
+}
---
+++ src/test/kotlin/com/kaizenflow/techtales/TechTalesApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.kaizenflow.techtales
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class TechTalesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
